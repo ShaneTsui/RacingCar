60e8c64c58d8384e5ca2253ec5f0ddb3cf004ee3
ShaneTsui <shanetsui.cs@gmail.com>
2019-11-25 23:58:56 -0800

    Initialized simple policy

---
+++ policy/simple_policy.py
@@ -0,0 +1,22 @@
+from torch import nn as nn
+from torch.nn import functional as F
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        """
+        Initialize the network
+        param: state_dim : 2N + 3: 2N = N + N, extra: v_prev, a_prev, steer_prev
+        param: action_dim: 2N = N + N, [Xs, Ys]
+        """
+        super(PolicyNetwork, self).__init__()
+        self.linear1 = nn.Linear(state_dim, 256)
+        self.linear2 = nn.Linear(256, action_dim)
+
+    def forward(self, state):
+        """
+        Define the forward pass
+        param: state: The state of the environment
+        """
+        x = F.relu(self.linear1(state))
+        x = self.linear2(x)
+        return x
